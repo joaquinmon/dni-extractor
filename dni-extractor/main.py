c27905f7cedf2b3b5dc1c27dafe36329fa914450
Joaquin M <joaquin.monterocarbonero@colegio-losnaranjos.com>
2023-11-03 19:51:35 +0100

    added the core script

--- dni-extractor/main.py
+++ dni-extractor/main.py
@@ -1,4 +1,19 @@
+#!/usr/bin/env python3
 
-def main():  # pragma: no cover
+def calculate_dni_letter(number):
+    letters = "TRWAGMYFPDXBNJZSQVHLCKE"
+    return letters[number % 23]
 
-    print("This will do something")
+def generate_dni(name):
+  ascii_sum = sum(ord(char) for char in name if char.isalnum())
+
+  number_part = (ascii_sum * 1234 + 67890) % 100000000
+
+  letter = calculate_dni_letter(number_part)
+
+  return f"{number_part:08d}{letter}"
+
+name = input("Introduce tu nombre completo: ")
+
+dni = generate_dni(name)
+print(f"El DNI del usuario es: {dni}")
